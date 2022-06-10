7a5e889c671d5b7cb9d56b3c453a4067baaa39db
Cleonildo Jr <cleu.junior@gmail.com>
2022-06-09 22:07:19 -0300

    Initital Project

---
+++ src/main/java/com/padotec/coding/tests/PadotecCodingTestsApplication.java
@@ -0,0 +1,13 @@
+package com.padotec.coding.tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadotecCodingTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadotecCodingTestsApplication.class, args);
+	}
+
+}
