d693c9c055a2507c95ceea8000fed66aad91ce3a
CleuJunior <cleu.junior@gmail.com>
2023-11-03 16:17:35 -0300

    refactor: refact folder.

--- src/main/java/com/padotec/coding/tests/repositories/IoTDeviceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.padotec.coding.tests.repositories;
-
-import com.padotec.coding.tests.entities.IoTDevice;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IoTDeviceRepository extends JpaRepository<IoTDevice, Long> {
-}
--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO iot_device(device_id, name, mac, email, latitude, longitude) VALUES (null, 'Laboratório', 'B7-92-EE-8F-34-AA', 'laboratorio@gmail.com', 9989.98, 7758.99)
-INSERT INTO iot_device(device_id, name, mac, email, latitude, longitude) VALUES (null, 'Sala de Testes', '29-4E-63-96-FA-1', 'saladetestes@gmail.com', 789.31, 1058.82)
-INSERT INTO iot_device(device_id, name, mac, email, latitude, longitude) VALUES (null, 'Área de Manutenção', '5D-A6-36-7A-77-43', 'manutencaoarea@gmail.com', 189.11, 758.12)
